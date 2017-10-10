e684b0b520cf0614f08ab8f635e9acd7df2dbc90
Aman <aman.gaur1011@gmail.com>
2017-10-10 09:11:56 +0530

    Making movie.py compatible with PE8
    Making movie.py compatible with PE8

--- movie.py
+++ movie.py
@@ -1,15 +1,25 @@
 import webbrowser
+
+
 class Movie():
 
     # declaring the documentation.
-    """ This is a class made for Udacity's Full Stack NanoDegree programme's first project """
+    """ This is a class made for Udacity's Full
+        Stack NanoDegree programme's first project
+    """
     # declaring a claas variable.
 
     # Every class has a init function
     # otherwise known as constructor
     # which gets called upon while creating
     # an instance or object.
-    def __init__(self, movie_title, movie_storyline, movie_poster_image, movie_trailer):
+    def __init__(
+        self,
+        movie_title,
+        movie_storyline,
+        movie_poster_image,
+        movie_trailer
+    ):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster_image
